99b5c7b621c52c167b7ded3004fd42bb77e693af
Bobby Ren <bobbyren@gmail.com>
2015-06-14 20:16:26 -0400

    added menu button and swipe for navigation

--- RecipeList/MainViewController.swift
+++ RecipeList/MainViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 
     override func didReceiveMemoryWarning() {
