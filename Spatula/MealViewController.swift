60d661e47c1772930006832b8636aba4838a6daf
Hana Kim <hana@addepar.com>
2015-09-27 16:56:02 -0400

    getting ingredients table to work

--- Spatula/MealViewController.swift
+++ Spatula/MealViewController.swift
@@ -8,21 +8,35 @@
 
 import UIKit
 
-class MealViewController: UIViewController {
+class MealViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var recipe: Recipe!
     var recipeType: Int!
     var selectedTab: Int = 0
     var viewWidth: CGFloat!
     
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealCalories: UILabel!
+    @IBOutlet weak var mealPrice: UILabel!
+    @IBOutlet weak var mealDifficulty: UILabel!
+    
     @IBOutlet weak var detailsImage: UIImageView!
     @IBOutlet weak var detailsTab: UIButton!
     @IBOutlet weak var ingredientsTab: UIButton!
     @IBOutlet weak var detailsContent: UIView!
+    @IBOutlet weak var mealDescriptionLabel: UILabel!
     @IBOutlet weak var ingredientsContent: UIView!
-    @IBOutlet var ingredientsTrailingConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var ingredientsTableView: UITableView!
+    
+    
+    @IBOutlet var ingredientsTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var selectedTabBorderLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var selectedTabBorder: UIView!
+    
+    var ingredients: [String] = ["5 tablespoons","2 ounces","a pinch","4","40 mL", "5 tablespoons","2 ounces","a pinch","4","40 mL"]
+    var ingredientAmounts: [String] = ["Sugar","Oil","Cinimon","Eggs","Water","Something Else","Sea Salt","Juice","Oranges","Apple Juice"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,15 +61,17 @@ class MealViewController: UIViewController {
         // load info for recipe
         detailsImage.image = recipe.image
         detailsImage.clipsToBounds = true
-        nameLabel.text = recipe.name
+        
+        mealNameLabel.text = recipe.name
+        mealCalories.text = String(recipe.calories) + " kCal"
+        mealDescriptionLabel.text = recipe.longDescription
+        
         
         detailsTab.tag = 0
         ingredientsTab.tag = 1
         
         self.ingredientsTrailingConstraint.constant = -20 - viewWidth
-        self.view.layoutIfNeeded()
-        
-        //print("view Width: \(viewWidth)")
+        self.view.layoutIfNeeded()        
     }
     
     
@@ -73,6 +89,7 @@ class MealViewController: UIViewController {
         self.view.layoutIfNeeded()
         UIView.animateWithDuration(0.3) { () -> Void in
             self.ingredientsTrailingConstraint.constant = -20 - self.viewWidth
+            self.selectedTabBorderLeadingConstraint.constant = 0
             self.view.layoutIfNeeded()
         }
         
@@ -83,12 +100,35 @@ class MealViewController: UIViewController {
         self.view.layoutIfNeeded()
         UIView.animateWithDuration(0.3) { () -> Void in
             self.ingredientsTrailingConstraint.constant = -20.0
+            self.selectedTabBorderLeadingConstraint.constant = (self.viewWidth / 2)
             self.view.layoutIfNeeded()
         }
-        
         selectedTab = 1
-        
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredients.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("IngredientCell")
+        let row = indexPath.row
+
+        let ingredientNameLabel: UILabel = cell!.viewWithTag(1) as! UILabel
+        let ingredientAmountLabel: UILabel = cell!.viewWithTag(2) as! UILabel
+        
+        ingredientNameLabel.text = ingredients[row]
+        ingredientAmountLabel.text = ingredientAmounts[row]
+        print(ingredients[row])
+        
+        return cell!
+    }
+
+
     
     
     /*
